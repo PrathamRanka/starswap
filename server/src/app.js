8bc48fd66a5def7c7ad2558b86ad2a6bb2203f18
Pratham <pranka0789@gmail.com>
2026-02-27 17:25:54 +0530

    added cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import cors from 'cors';
 import sessionConfig from './config/session.js';
 import errorMiddleware from './middleware/error.middleware.js';
 
 const app = express();
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  process.env.FRONTEND_URL_TEST
+].filter(Boolean); // Filters out any undefined/empty variables
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // Crucial for sending secure HttpOnly session cookies
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
