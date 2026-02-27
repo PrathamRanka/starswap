329c530309283fe9095f4c93fe16ac5b6dee26ee
Pratham <pranka0789@gmail.com>
2026-02-28 01:46:26 +0530

    added console.log for debugging

--- server/src/server.js
+++ server/src/server.js
@@ -5,8 +5,10 @@ import startAbuseMonitorJob from './jobs/abuseMonitor.job.js';
 import startLeaderboardRecalcJob from './jobs/leaderboardRecalc.job.js';
 import logger from './utils/logger.js';
 
-const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 5000;
+console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
+console.log("DATABASE_URL value:", process.env.DATABASE_URL);
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
   
